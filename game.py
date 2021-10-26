a35609ceec9d0d028747e04c7e5498887bb890f3
Finn-Jones1 <47406390+finnjones@users.noreply.github.com>
2021-10-26 21:59:29 +1100

    new stuff

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import os
 import random
+import math
 from cards import *
 pygame.init()
 
@@ -13,54 +14,74 @@ red = (255, 0, 0)
 grey = (128,128,128)
 
 clock = pygame.time.Clock()
+vec = pygame.math.Vector2
 FlexyPath = os.path.dirname(os.path.abspath(__file__))
 screenSize = (1620 , 1000)
 
 window = pygame.display.set_mode(screenSize)
+cardsL = []
 
-
-cardBack = pygame.transform.scale(pygame.image.load(FlexyPath + "/Cards/Back/red.png"), (150, 213))
-
+print(list(cardDict)[0])
+cardSelector = 0
 class cards(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, pos):
         self.x = x
         self.y = y
         self.vel = 10
         self.rVel = 4
         self.friction = 0.05
         self.rFriction = 0.01
+        self.pos = pos
         self.rot = 0
-
+        self.cardBack = pygame.transform.scale(pygame.image.load(FlexyPath + "/Cards/Front/" + list(cardShuffle.d)[cardSelector] + ".png").convert_alpha(), (150, 213))
+        cardsL.append(self)
     def slideCards(self):
-        if self.vel > 0:
-            self.vel -= self.friction
-            self.rVel -= self.rFriction
+
+
+        self.rot = (vec(self.x, self.y) - vec(self.pos)).angle_to(vec(1, 0)) + 90
+        print(self.rot)
+        if self.x < self.pos[0]:
+            self.x += self.vel
+        elif self.x > self.pos[0]:
+            self.x -= self.vel
+        if self.y < self.pos[1]:
             self.y += self.vel
-            self.rot += self.rVel
+        elif self.y > self.pos[1]:
+            self.y -= self.vel
+            # self.vel -= self.friction
+            # self.rVel -= self.rFriction
+            # self.y += self.vel
+            # self.rot += self.rVel
 
     def draw(self):
-        
-        
-        img = pygame.transform.rotate(cardBack, self.rot)
+        img = pygame.transform.rotate(self.cardBack, self.rot)
         window.blit(img, (self.x - int(img.get_width() / 2), self.y - int(img.get_height() / 2)))
 
         # window.blit(pygame.transform.rotate(cardBack, self.rot), (self.x, self.y))
 
+class cardShuffle():
+    def shuffle(self):
+        self.l = list(cardDict.items())
+        random.shuffle(self.l)
+        self.d = dict(self.l)
 
 def redraw():
     window.fill(white)
-    cards.draw()
+    for i in cardsL:
+        i.draw()
+    
     pygame.display.flip()
 
-def shuffle(self):
-    l = list(cardDict.items())
-    random.shuffle(l)
-    d = dict(l)
-    print(d)
+
 
 
 running = True
 spin = False
+cardShuffle = cardShuffle()
+cardShuffle.shuffle()
+cards(screenSize[0]/2, 0, (900,900))
+
+
 while running:
     redraw()
     clock.tick(60)
@@ -74,8 +95,12 @@ while running:
         
         if keys[pygame.K_SPACE]:
             spin = True
+            cardSelector += 1
+
+            cards(screenSize[0]/2, 0, (900,900))
     if spin == True:
-        cards.slideCards()
+        for i in cardsL:
+            i.slideCards()
     
 
 pygame.quit()
