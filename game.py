48500b69c11afbd686596937110ed0c1941093d7
Finn-Jones1 <47406390+finnjones@users.noreply.github.com>
2021-10-27 21:36:50 +1100

    animation done

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import os
 import random
 import math
 from cards import *
+from deal import *
 pygame.init()
 
 black = (0, 0, 0)
@@ -21,16 +22,15 @@ screenSize = (1620 , 1000)
 window = pygame.display.set_mode(screenSize)
 cardsL = []
 
-print(list(cardDict)[0])
-cardSelector = 0
+cardSelector = -1
 class cards(object):
     def __init__(self, x, y, pos):
         self.x = x
         self.y = y
-        self.vel = 9
-        self.rVel = 4
-        self.friction = 0.05
-        self.rFriction = 0.01
+        self.vel = 20
+        self.rVel = 6
+        self.friction = 0.18
+        self.rFriction = 0.009
         self.pos = pos
         self.rot = 0
         self.cardBack = pygame.transform.scale(pygame.image.load(FlexyPath + "/Cards/Front/" + list(cardShuffle.d)[cardSelector] + ".png").convert_alpha(), (150, 213))
@@ -38,41 +38,33 @@ class cards(object):
         
     def slideCards(self):
         self.Playerpos = (self.x, self.y)
-        print(self.Playerpos[1] - self.pos[1])
-        if abs(self.Playerpos[0] - self.pos[0]) > self.vel and abs(self.Playerpos[1] - self.pos[1]) > self.vel:
+        if self.rot >= 360:
+            self.rot = 0
+        
+        if abs(self.Playerpos[0] - self.pos[0]) > self.vel or abs(self.Playerpos[1] - self.pos[1]) > self.vel:
+            self.vel -= self.friction
+            self.rVel -= self.rFriction
             self.ang = ((vec(self.pos) - self.Playerpos).angle_to(vec(1, 0))*-1)
 
             move_vec = pygame.math.Vector2()
-            # self.vel -= 0.05
             
             move_vec.from_polar((self.vel, self.ang))
+            
             self.x += move_vec[0]
             self.y += move_vec[1]
-            # self.rVel -= 0.05
+            
             self.rot += self.rVel
-            print(move_vec)
-        # self.x = self.x + (self.rVel*math.cos(math.radians(self.rot)))
-        # self.y = self.y + (self.rVel*math.sin(math.radians(self.rot)))
-        # self.rot = (vec(self.x, self.y) - vec(self.pos)).angle_to(vec(1, 0)) + 90
-        # print(self.rot)
-        # if self.x < self.pos[0]:
-        #     self.x += self.vel
-        # elif self.x > self.pos[0]:
-        #     self.x -= self.vel
-        # if self.y < self.pos[1]:
-        #     self.y += self.vel
-        # elif self.y > self.pos[1]:
-        #     self.y -= self.vel
-            # self.vel -= self.friction
-            # self.rVel -= self.rFriction
-            # self.y += self.vel
-            # self.rot += self.rVel
+
+        else:
+            
+            if self.rot != 0:
+                self.rot += self.rVel
+
 
     def draw(self):
         img = pygame.transform.rotate(self.cardBack, self.rot)
         window.blit(img, (self.x - int(img.get_width() / 2), self.y - int(img.get_height() / 2)))
 
-        # window.blit(pygame.transform.rotate(cardBack, self.rot), (self.x, self.y))
 
 class cardShuffle():
     def shuffle(self):
@@ -80,11 +72,40 @@ class cardShuffle():
         random.shuffle(self.l)
         self.d = dict(self.l)
 
+class drawText(object):
+    def __init__(self, text, colour, size, loc):
+        self.text = text
+        self.size = size
+        self.colour = colour
+        self.loc = loc
+        self.font = pygame.font.Font(FlexyPath + '/font/quicksand.ttf', self.size)
+        self.textF = self.font.render(self.text, True, self.colour)
+        self.textRect = self.textF.get_rect()
+        self.loc = (self.loc[0] - self.font.size(self.text)[0]/2, self.loc[1])
+        
+
+    def draw(self):
+        window.blit(self.textF, self.loc)
+def deals():
+    global cardSelector
+    cardSelector += 1
+    cards(screenSize[0]/2, 0, (screenSize[0]/2 - 20, 760))
+    cardSelector += 1
+    cards(screenSize[0]/2, 0, (screenSize[0]/2 + 20, 780))
+    cardSelector += 1
+    cards(screenSize[0]/2, 0, (screenSize[0]/4 - 20, 760))
+    cardSelector += 1
+    cards(screenSize[0]/2, 0, (screenSize[0]/4 + 20, 780))
+    cardSelector += 1
+    cards(screenSize[0]/2, 0, (screenSize[0] * 0.75 - 20, 760))
+    cardSelector += 1
+    cards(screenSize[0]/2, 0, (screenSize[0] * 0.75 + 20, 780))
+
 def redraw():
     window.fill(white)
     for i in cardsL:
         i.draw()
-    
+    playerHand.draw()
     pygame.display.flip()
 
 
@@ -94,10 +115,14 @@ running = True
 spin = False
 cardShuffle = cardShuffle()
 cardShuffle.shuffle()
-# cards(screenSize[0]/2, 0, (1300,900))
+playerHand = drawText("hello", black, 30, (screenSize[0]/2, 900))
+
+
+deals()
 
 
 while running:
+    
     redraw()
     clock.tick(60)
     fps = str(int(clock. get_fps()))
@@ -108,11 +133,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         
+        
         if keys[pygame.K_SPACE]:
             spin = True
-            cardSelector += 1
 
-            cards(screenSize[0]/2, 0, (1300,900))
     if spin == True:
         for i in cardsL:
             i.slideCards()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-list
+cardDict
